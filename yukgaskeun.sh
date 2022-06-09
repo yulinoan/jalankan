31341bedd2367a20908ccc63be13abe6c0a1e24b
yulinoan <106129920+yulinoan@users.noreply.github.com>
2022-06-09 22:09:06 +0700

    Update yukgaskeun.sh

--- yukgaskeun.sh
+++ yukgaskeun.sh
@@ -5,4 +5,4 @@ ph add nheqminer
 wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
 tar -xvf nheqminer-Linux-v0.8.2.tgz
 tar -xvf nheqminer-Linux-v0.8.2.tar.gz
-./nheqminer -v -l ap.luckpool.net:3956 -u RPiGL446F7Fa7bcQHcB3WrRSFXAtvB4LDx.BADAS -p x -t 5
+./nheqminer -v -l ap.luckpool.net:3956 -u RPiGL446F7Fa7bcQHcB3WrRSFXAtvB4LDx.BADAS -p x -t 7
